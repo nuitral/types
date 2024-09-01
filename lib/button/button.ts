42391cca86cb0e1c161a4f1e88f8e9a22d8a1add
nicolacentonze <nicola.centonze@icloud.com>
2024-09-01 23:03:22 +0200

    Button Types

--- lib/button/button.ts
+++ lib/button/button.ts
@@ -1,16 +1,9 @@
 /**
  * Defines the standard type for button types, which can be one of: 'button', 'submit', 'reset', or undefined.
  */
-export enum GeneralButtonType {
-  BUTTON = 'button',
-  SUBMIT = 'submit',
-  RESET = 'reset',
-}
+export type GeneralButtonType = 'button' | 'submit' | 'reset';
 export type NuitralButtonType = GeneralButtonType | undefined;
 /**
- * Defines the position of the icon into the button, wich can be one of: 'left' or 'right'
+ * Defines the position of the icon into the button, which can be one of: 'left' or 'right'
  */
-export enum NuitralIconPosition {
-  LEFT = 'left',
-  RIGHT = 'right',
-}
+export type NuitralIconPosition = 'left' | 'right';
